1b5b206802200f9398cbff667e44ef00704e7e40
roshdy1221985 <roshdysayed751991@gmail.com>
2026-04-25 02:25:49 +0400

    new42

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ ALLOWED_FILE_EXTENSIONS = {
     ".mp4", ".webm", ".mp3", ".wav", ".m4a",
 }
 
-MAX_FILE_SIZE_MB = 10  # الحد الأقصى لحجم الملف بالميجابايت
+MAX_FILE_SIZE_MB = 30  # الحد الأقصى لحجم الملف بالميجابايت
 MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024
 
 
